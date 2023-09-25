67c8daf44a6d437dbc9005f31e71b91a3129b9ef
nickcastela2022@hotmail.com <nickcastela2022@hotmail.com>
2023-09-25 17:42:13 -0400

    aula 15 - terminar

---
+++ src/components/MultiplosEventos.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>oasdadsi</p>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name:'MultiplosEventos'
+}
+</script>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -2,13 +2,17 @@
     <div>
         <h1>Ola Vue!!</h1>
         <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
-    </div>
+        <MultiplosEventos/>
+   </div>
 </template>
 
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
+
 export default {
     name:'PrimeiroComponente',
+    components:{MultiplosEventos},
     data(){
         return{
             nome: 'Nicolas',
