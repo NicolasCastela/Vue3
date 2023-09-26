e29dbb35660fe4b04f7041b8125db6f51362a956
nickcastela2022@hotmail.com <nickcastela2022@hotmail.com>
2023-09-26 09:49:19 -0400

    I learned how to use events and multiple events

--- src/components/MultiplosEventos.vue
+++ src/components/MultiplosEventos.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <p>oasdadsi</p>
+        <button @click="primeiro(txt1, $event), segundo(txt2,$event), terceiro(txt3, $event)"> Ativar multiplos eventos</button>
+        <p>{{ m_p }}</p>
     </div>
 </template>
 
@@ -8,6 +9,39 @@
 
 <script>
 export default {
-    name:'MultiplosEventos'
+    name:'MultiplosEventos',
+
+data() {
+    return {
+        m_p:'',
+        txt1: 'Primeiro Evento',
+        txt2: 'Segundo Evento ',
+        txt3: ''
+    }
+},
+methods:{
+primeiro(txt1, e){
+   this.m_p = txt1;
+    
+},
+segundo(txt2, e){
+    setTimeout(()=> { this.m_p = txt2;
+
+     },1000)
+},
+terceiro(txt3,e){
+    setTimeout(()=> this.m_p = txt3, 2000)
 }
+
+}
+}
+
+
+
 </script>
+
+<style scoped>
+div{
+    margin: 50px;
+}
+</style>
