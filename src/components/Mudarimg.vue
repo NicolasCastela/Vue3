3951752ff2d93204ca6381caf1108223bf1ce284
NicolasCastela <nickcastela2022@hotmail.com>
2023-09-30 12:12:59 -0300

    How to use $emit in vue3

---
+++ src/components/Mudarimg.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <div>
+<button @click="$emit('Mudarimg')">Trocar img de perfil </button>
+    </div>
+</template>
+
+<script>
+export default{
+name:'Mudarimg',
+emits:['Mudarimg']
+
+
+}
+</script>
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,19 +1,29 @@
 <template>
     <div>
       <img :src="avatar" :alt="descricao">
+      <Mudarimg @mudarimg="trocarimg"/>
     </div>
 </template>
 
 <script>
+import Mudarimg from './Mudarimg.vue';
+
 export default {
-name:'Picture',
-data(){
-    return{
-        avatar:'/img/avatar.png',
-        descricao:'Bob sponja'
-    }
-}
-
-
+    name: 'Picture',
+    data() {
+        return {
+            avatar: '/img/avatar.png',
+            descricao: 'Bob sponja'
+        };
+    },
+    components: { Mudarimg },
+    methods: {
+        trocarimg(){
+          this.avatar = "/img/avatar2.png"
+          setTimeout(()=> {
+            this.avatar="img/avatar.png"}, 2000 )
+        }
+        
+    },
 }
 </script>
